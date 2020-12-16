05631e80c31830e89b2df51fa4d18f564f1056c7
TimGrogan <twgrogan13@gmail.com>
2020-12-16 15:00:08 -0500

    save changes

---
+++ lab 3.2.1.6.py
@@ -0,0 +1,11 @@
+import time
+seconds = 0
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
+for seconds in range (1,6):
+    print(seconds,"Mississippi")
+    seconds = seconds + 1
+print("Ready or not, here I come!")
