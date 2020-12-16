d00de162a84fe5599350b0fda0a77628a7cf5d18
TimGrogan <twgrogan13@gmail.com>
2020-12-16 15:50:43 -0500

    save

--- Im learning python.py
+++ Im learning python.py
@@ -1,3 +1,15 @@
-fahrenheit = 32
-celsius = round((fahrenheit -32) * 5 / 9,2)
-print(fahrenheit,"degrees F =",celsius,"degrees C")
+largest_number = -99999999
+counter = 0
+
+while True:
+    number = int(input("Enter a number or type -1 to end program: "))
+    if number == -1:
+        break
+    counter += 1
+    if number > largest_number:
+        largest_number = number
+
+if counter != 0:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
---
+++ lab 3.2.1.9.py
@@ -0,0 +1,6 @@
+animal = input("Enter an animal name: ")
+while animal != "chupacabra":
+   if animal == "chupac":
+       break
+   animal = input("enter another: ")
+print("You entered a mythical mexican beast!")
